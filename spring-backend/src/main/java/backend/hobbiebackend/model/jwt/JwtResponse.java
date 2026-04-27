18a68ddf4701aefda567250b5ed69531c21fece9
mohan612 <84223204+mohan612@users.noreply.github.com>
2026-04-27 20:59:56 +0530

    update changes

--- spring-backend/src/main/java/backend/hobbiebackend/model/jwt/JwtResponse.java
+++ spring-backend/src/main/java/backend/hobbiebackend/model/jwt/JwtResponse.java
@@ -1,12 +1,18 @@
 package backend.hobbiebackend.model.jwt;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class JwtResponse {
+
     private String jwtToken;
+
+    public JwtResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
 }
